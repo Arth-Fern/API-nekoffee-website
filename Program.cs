11c179de7819dcfc871be1d2232b00e7e47b5e7a
Arthur Fernandes de Sousa <Arturfernan42@gmail.com>
2026-05-18 20:00:14 -0300

    primeiro commit API

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using nekoffee.Data;
+using nekoffee.EndPoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ClientesContexto>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.AllowAnyOrigin()              
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Middlewares - Comunicação com o JS
+
+app.UseCors("AllowFrontend");               
+app.MapClientesEndPoints();                 
+app.MapControllers();                       
+
+app.Run();                                  
